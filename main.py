eb040f8d9764b6d48193571ebaf4a9d3cd168154
Kedarkd12 <kedar.antarkar@gmail.com>
2026-07-30 22:11:50 +0530

    Implement CSV validation and data cleaning pipeline

--- main.py
+++ main.py
@@ -10,98 +10,142 @@ def load_data():
             employees.append(row)
     return employees
 
-def validate_id(records):
-    colleague=set()
+def validate_data(records):
+    invalid_records=[]
+    valid_records=[]
+    seen_ids=set()
     missing_employee_id=0
     duplicate_employee_id=0
-    for record in records:
-        e_id = record["Employee_ID"].strip()
-        if e_id == "":
-            missing_employee_id+=1
-        else:
-            if e_id not in colleague:
-                colleague.add(e_id)
-            else:
-                duplicate_employee_id+=1
-    return missing_employee_id,duplicate_employee_id
-
-def validate_name(records):
     missing_employee_name=0
-    for record in records:
-        if record["Employee_Name"].strip() == "":
-            missing_employee_name+=1
-    return missing_employee_name
-
-def validate_department(records):
     missing_employee_department=0
-    for record in records:
-        if record["Department"].strip() == "":
-            missing_employee_department+=1
-    return missing_employee_department
-
-def validate_age(records):
     invalid_age=0
-    for record in records:
-        if record["Age"].strip() != "":
-            try:
-                age = int(record["Age"])
-                if not 18 < age < 70:
-                    invalid_age+=1
-            except ValueError:
-                invalid_age+=1
-        else:
-            invalid_age+=1
-    return invalid_age
-
-def validate_salary(records):
     invalid_salary=0
-    for record in records:
-        if record["Salary"].strip() != "":
-            try:
-                salary = float(record["Salary"])
-                if salary <= 0:
-                    invalid_salary+=1
-            except ValueError:
-                invalid_salary+=1
-        else:
-            invalid_salary+=1
-    return invalid_salary
-
-def validate_date(records):
     invalid_date=0
     for record in records:
-        if record["Joining_Date"].strip() != "":
-            try:
-                join_date = datetime.date.fromisoformat(record["Joining_Date"])
-            except ValueError:
-                invalid_date+=1
+        if record["Employee_ID"] not in seen_ids:
+            seen_ids.add(record["Employee_ID"])
+            duplicate_eid=False
         else:
+            duplicate_employee_id+=1
+            duplicate_eid = True
+        eid = validate_id(record)
+        if eid == False:
+            missing_employee_id+=1
+        name = validate_name(record)
+        if name == False:
+            missing_employee_name+=1
+        dept = validate_department(record)
+        if dept == False:
+            missing_employee_department+=1
+        age = validate_age(record)
+        if age == False:
+            invalid_age+=1
+        salary = validate_salary(record)
+        if salary == False:
+            invalid_salary+=1
+        date = validate_date(record)
+        if date == False:
             invalid_date+=1
-    return invalid_date
+        if not duplicate_eid and eid and name and dept and age and salary and date == True:
+            valid_records.append(record)
+        else:
+            invalid_records.append(record)
+    return valid_records,invalid_records,missing_employee_id,missing_employee_name,missing_employee_department,invalid_age,invalid_salary,invalid_date,duplicate_employee_id
+
+def validate_id(record):
+    valid = True
+    e_id = record["Employee_ID"].strip()
+    if e_id == "":
+        valid = False
+    return valid
+
+def validate_name(record):
+    valid = True
+    if record["Employee_Name"].strip() == "":
+        valid = False
+    return valid
+
+def validate_department(record):
+    valid = True
+    if record["Department"].strip() == "":
+        valid = False
+    return valid
+
+def validate_age(record):
+    valid = True
+    if record["Age"].strip() != "":
+        try:
+            age = int(record["Age"])
+            if not 18 < age < 70:
+                valid = False
+        except ValueError:
+            valid = False
+    else:
+        valid = False
+    return valid
+
+def validate_salary(record):
+    valid = True
+    if record["Salary"].strip() != "":
+        try:
+            salary = float(record["Salary"])
+            if salary <= 0:
+                valid = False
+        except ValueError:
+            valid = False
+    else:
+        valid = False
+    return valid
+
+def validate_date(record):
+    valid = True
+    if record["Joining_Date"].strip() != "":
+        try:
+            join_date = datetime.date.fromisoformat(record["Joining_Date"])
+        except ValueError:
+            valid = False
+    else:
+        valid = False
+    return valid
 
 def seperate_records(records):
     pass
 
-def write_cleaned_csv(records):
-    pass
+def write_cleaned_csv(correct):
+    with open("data/cleaned/cleaned.csv","w",newline="") as file:
+        writer = csv.DictWriter(file,fieldnames=["Employee_ID","Employee_Name","Age","Country","Department","Position","Salary","Joining_Date"])
+        writer.writeheader()
+        for row in correct:
+            writer.writerow(row)
 
-def write_rejected_csv(records):
-    pass
+def write_rejected_csv(incorrect):
+    with open("data/rejected/rejected.csv","w",newline="") as file:
+            writer = csv.DictWriter(file,fieldnames=["Employee_ID","Employee_Name","Age","Country","Department","Position","Salary","Joining_Date"])
+            writer.writeheader()
+            for row in incorrect:
+                writer.writerow(row)
 
 def invalid_data_report(data):
     print("Validation report")
     print("------------------")
-    print("Missing Employee IDs:",validate_id(data)[0])
-    print("Missing Employee Name:",validate_name(data))
-    print("Missing Employee Department:",validate_department(data))
-    print("Invalid Age:",validate_age(data))
-    print("Invalid Salary:",validate_salary(data))
-    print("Invalid Date Format:",validate_date(data))
-    print("Duplicate Employee IDs",validate_id(data)[1])
+    print("Valid Records:",len(data[0]))
+    print("Invalid Records:",len(data[1]))
+    print("Missing Employee IDs:",data[2])
+    print("Missing Employee Name:",data[3])
+    print("Missing Employee Department:",data[4])
+    print("Invalid Age:",data[5])
+    print("Invalid Salary:",data[6])
+    print("Invalid Date Format:",data[7])
+    print("Duplicate Employee IDs",data[8])
 
 def main():
     records=load_data()
-    invalid_data_report(records)
+    data = validate_data(records)
+    correct_data = data[0]
+    incorrect_data = data[1]
+    invalid_data_report(data)
+    write_cleaned_csv(correct_data)
+    write_rejected_csv(incorrect_data)
 
 """#debugging
     print(len(records))
